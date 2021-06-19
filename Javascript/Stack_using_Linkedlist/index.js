9380622572df84704591d879b9c1fcc7f4b74401
Abdullah Turky <abdullahturki087@gmail.com>
2021-06-19 13:06:46 +0600

    created stack using Linked list

---
+++ Javascript/Stack_using_Linkedlist/index.js
@@ -0,0 +1,67 @@
+console.clear();
+
+// Implementing Stack using Linkedlist
+
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class Stack{
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek(){
+        console.log(this.top);
+        return this.top;
+    }
+    
+    push(value){
+        const newNode = new Node(value);
+        if(this.length == 0){
+            this.top = newNode;
+            this.bottom = newNode;
+        }
+        else{
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(!this.top){
+            return null;
+        }
+        if(this.top === this.bottom)
+        {
+            this.bottom = null;
+        }
+        const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+
+    toStr() {
+        return JSON.stringify(this);
+    }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.pop();
+
+stack.peek();
+console.log("------------------------------------------------------------------");
+console.log(stack.toStr());
